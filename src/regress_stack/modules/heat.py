e476e80074272206c0f5733fc5c5bb406ca6fbe1
Felipe Reyes <felipe.reyes@canonical.com>
2026-02-11 18:59:56 -0300

    heat: migrate services to use apache2
    During the Flamingo release the services heat-api and heat-api-cfn were
    migrated to be run as a wsgi app under apache2
    
    Signed-off-by: Felipe Reyes <felipe.reyes@canonical.com>

--- src/regress_stack/modules/heat.py
+++ src/regress_stack/modules/heat.py
@@ -4,6 +4,7 @@
 import logging
 import pathlib
 
+from regress_stack.core import apt as core_apt
 from regress_stack.core import utils as core_utils
 from regress_stack.modules import keystone, mysql, neutron, nova, rabbitmq
 from regress_stack.modules import utils as module_utils
@@ -22,6 +23,7 @@ SERVICE = "heat"
 SERVICE_CFN = "heat-cfn"
 SERVICE_TYPE = "orchestration"
 SERVICE_TYPE_CFN = "cloudformation"
+HEAT_FLAMINGO_VERSION = "1:25.0.0~rc1-0ubuntu3"
 HEAT_STACK_ADMIN = "heat_admin"
 HEAT_STACK_ADMIN_PASSWORD = "changeme"
 
@@ -98,9 +100,15 @@ def setup():
         ),
     )
     core_utils.sudo("heat-manage", ["db_sync"], user=SERVICE)
-    core_utils.restart_service("heat-api")
-    core_utils.restart_service("heat-api-cfn")
-    core_utils.restart_service("heat-engine")
+    heat_daemons = ["heat-api", "heat-api-cfn", "heat-engine"]
+    if core_apt.PkgVersionCompare("python3-heat") >= HEAT_FLAMINGO_VERSION:
+        heat_daemons.remove("heat-api")
+        heat_daemons.remove("heat-api-cfn")
+        # heat-api and heat-api-cfn run as WSGI apps under apache2.
+        heat_daemons.insert(0, "apache2")
+
+    for _daemon in heat_daemons:
+        core_utils.restart_service(_daemon)
 
 
 def configure_tempest(tempest_conf: pathlib.Path):
